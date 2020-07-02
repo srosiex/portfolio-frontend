368ae9fb56364ced86c6d3ef386b6d8844d792e1
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-07-02 17:33:33 -0500

    removed mdl styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import {Grid, Tabs, Tab, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText} from 'react-mdl'
 import {cookbookss} from '../photos/cookbookss.png'
 
 
@@ -17,90 +16,90 @@ export class Projects extends Component {
             return(
                 //Project 1 //
                 <div className="projects-grid">
-                <Card shadow={5} style={{minWidth:'450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#fff', height:'176px', background: 'url({cookbookss}) center / cover '}}>
+                <div shadow={5} style={{minWidth:'450', margin: 'auto'}}>
+                    <div style={{color: '#fff', height:'176px', background: 'url({cookbookss}) center / cover '}}>
                         Cookbook
-                    </CardTitle>
-                    <CardText>Developed a single page web app for users to input recipes, organize by their favorites, and add comments.
-                    </CardText>
-                    <CardActions border>
-                        <Button colored><a href="https://github.com/srosiex/cookbook-frontend" target="_blank">Github</a></Button>
-                        <Button colored><a href="https://cookbook-sh.netlify.app/" target="_blank">Live Demo</a></Button>
-                        <Button colored><a href="https://www.youtube.com/watch?v=n48req87c80&feature=youtu.be" target="_blank">Youtube</a></Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share" />
-                    </CardMenu>
-                </Card>
+                    </div>
+                    <div>Developed a single page web app for users to input recipes, organize by their favorites, and add comments.
+                    </div>
+                    <div border>
+                        <a href="https://github.com/srosiex/cookbook-frontend" target="_blank">Github</a>
+                        <a href="https://cookbook-sh.netlify.app/" target="_blank">Live Demo</a>
+                        <a href="https://www.youtube.com/watch?v=n48req87c80&feature=youtu.be" target="_blank">Youtube</a>
+                    </div>
+                    <div style={{color: '#fff'}}>
+                        <button name="share" />
+                    </div>
+                </div>
 
-                <Card shadow={5} style={{minWidth:'450', margin: 'auto'}}>
-                    <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover '}}>
+                <div shadow={5} style={{minWidth:'450', margin: 'auto'}}>
+                    <div style={{color: '#fff', height:'176px', background: 'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover '}}>
                         React Project 1
-                    </CardTitle>
-                    <CardText>asdflaksdjfa asdflaskdfalsdfj asdflkjasdkf</CardText>
-                    <CardActions border>
-                        <Button colored>Github</Button>
-                        <Button colored>Demo</Button>
-                        <Button colored>Youtube</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share" />
-                    </CardMenu>
-                </Card>
+                    </div>
+                    <div>asdflaksdjfa asdflaskdfalsdfj asdflkjasdkf</div>
+                    <div border>
+                        <button>Github</button>
+                        <button>Demo</button>
+                        <button>Youtube</button>
+                    </div>
+                    <div style={{color: '#fff'}}>
+                        <button name="share" />
+                    </div>
+                </div>
 
                 </div>
                 
             )
         }else if(this.state.activeTab === 1){
             return(
-                <Card shadow={5} style={{minWidth:'450', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover '}}>
+                <div shadow={5} style={{minWidth:'450', margin: 'auto'}}>
+                <div style={{color: '#fff', height:'176px', background: 'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover '}}>
                     React Project 1
-                </CardTitle>
-                <CardText>asdflaksdjfa asdflaskdfalsdfj asdflkjasdkf</CardText>
-                <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>Demo</Button>
-                    <Button colored>Youtube</Button>
-                </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                    <IconButton name="share" />
-                </CardMenu>
-            </Card>
+                </div>
+                <div>asdflaksdjfa asdflaskdfalsdfj asdflkjasdkf</div>
+                <div border>
+                    <button colored>Github</button>
+                    <button colored>Demo</button>
+                    <button colored>Youtube</button>
+                </div>
+                <div style={{color: '#fff'}}>
+                    <button name="share" />
+                </div>
+            </div>
             )
         }else if(this.state.activeTab === 2){
             return(
-                <Card shadow={5} style={{minWidth:'450', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover '}}>
+                <div shadow={5} style={{minWidth:'450', margin: 'auto'}}>
+                <div style={{color: '#fff', height:'176px', background: 'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover '}}>
                     React Project 1
-                </CardTitle>
-                <CardText>asdflaksdjfa asdflaskdfalsdfj asdflkjasdkf</CardText>
-                <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>Demo</Button>
-                    <Button colored>Youtube</Button>
-                </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                    <IconButton name="share" />
-                </CardMenu>
-            </Card>
+                </div>
+                <div>asdflaksdjfa asdflaskdfalsdfj asdflkjasdkf</div>
+                <div border>
+                    <button colored>Github</button>
+                    <button colored>Demo</button>
+                    <button colored>Youtube</button>
+                </div>
+                <div style={{color: '#fff'}}>
+                    <button name="share" />
+                </div>
+            </div>
             )
         }else if(this.state.activeTab === 3){
             return(
-                <Card shadow={5} style={{minWidth:'450', margin: 'auto'}}>
-                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover '}}>
+                <div shadow={5} style={{minWidth:'450', margin: 'auto'}}>
+                <div style={{color: '#fff', height:'176px', background: 'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover '}}>
                     React Project 1
-                </CardTitle>
-                <CardText>asdflaksdjfa asdflaskdfalsdfj asdflkjasdkf</CardText>
-                <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>Demo</Button>
-                    <Button colored>Youtube</Button>
-                </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                    <IconButton name="share" />
-                </CardMenu>
-            </Card>
+                </div>
+                <div>asdflaksdjfa asdflaskdfalsdfj asdflkjasdkf</div>
+                <div border>
+                    <button colored>Github</button>
+                    <button colored>Demo</button>
+                    <button colored>Youtube</button>
+                </div>
+                <div style={{color: '#fff'}}>
+                    <button name="share" />
+                </div>
+            </div>
             )
         }
     }
@@ -110,23 +109,24 @@ export class Projects extends Component {
 
         return (
             <div className="projects-body">
-                <div className="category-tabs">
-                    <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
-                        <Tab>React</Tab>
-                        <Tab>Javascript</Tab> 
-                        <Tab>Rails</Tab>
-                        <Tab>Sinatra</Tab>
-                    </Tabs>
+                <div className="category-tabs" activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} >
+                       <ul className="tabs"  >
+                       <li>Javascript</li> 
+                       <li>Rails</li>
+                       <li>Sinatra</li>
+                       <li>React</li>
+                       </ul>
+                </div>
                 {/* <section className="projects-grid"> */}
-                    <Grid>
-                        <Cell col={12}>
+                    <div>
+                        <div>
                             <div className="content">
                             {this.toggleCategories()}
                             </div>
-                        </Cell>
-                    </Grid>
+                        </div>
+                    </div>
                 {/* </section> */}
-            </div>
+          
             </div>
         )
     }
